13707f465e849b17e1658e6f165da3dbdf22ff6e
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-04-21 00:24:38 -0400

    Header 50% complete

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,32 @@
 import React from "react";
-import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  useScrollTrigger,
+  makeStyles,
+  Tabs,
+  Tab,
+} from "@material-ui/core";
+
+import logo from "../../assets/logo.svg";
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+}));
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -15,11 +42,25 @@ function ElevationScroll(props) {
 }
 
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>Arc Development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar disableGutters>
+            <img className={classes.logo} src={logo} alt="Company logo" />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home"></Tab>
+              <Tab className={classes.tab} label="Services"></Tab>
+              <Tab className={classes.tab} label="The Revolution"></Tab>
+              <Tab className={classes.tab} label="About Us"></Tab>
+              <Tab className={classes.tab} label="Contact Us"></Tab>
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin}></div>
+    </React.Fragment>
   );
 }
